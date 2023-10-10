1d1ce9539003be1d00bbf7ff6a1675a43f15902b
Cartiilov <karolina@DESKTOP-9BEL6PP.localdomain>
2023-10-10 18:11:44 +0200

    Random Planar Graph GENERATOR (2D)

--- src/PlotGraph.py
+++ src/PlotGraph.py
@@ -4,13 +4,14 @@ from planar_graph import PlanarTriangulation
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 
 
 def plot_graph(
-    graph: Graph, filepath: str | None = None, sp: Graph | None = None
+    graph: Graph, filepath: str | None = None, labels_dict: dict | None = None, sp: Graph | None = None
 ):
-    print(graph.to_adjacency_matrix())
+    #print(graph.to_adjacency_matrix())
     g = nx.from_numpy_array(graph.to_adjacency_matrix())
     if sp is not None:
         s = nx.from_numpy_array(sp.to_adjacency_matrix())
@@ -18,22 +19,115 @@ def plot_graph(
 
     plt.figure(figsize=(15, 15))
     position= nx.planar_layout(g, scale = 1, center=(0, 0))
-    nx.draw(g, pos=position)
-    nx.draw_networkx_labels(g, position)
-
-
-    nx.draw(g, pos=position)
+  
+    if labels_dict is not None:
+        #nx.draw_networkx_labels(labels_dict, position)
+        nx.draw(g, pos=position, labels = labels_dict, with_labels = True)
+    else:
+        nx.draw(g, pos=position)
+        nx.draw_networkx_labels(g, position)
 
     if filepath:
         plt.savefig(filepath)
     else:
         plt.show()
+        
+def plot_histogram(hist: np.histogram, filepath: str | None = None):
+    h, bin_edges = np.histogram(hist)
+    plt.figure(figsize=[10,8])
+    plt.hist(hist, bins=bin_edges)
+    # plt.bar(bin_edges[:-1], hist, width = 0.5, color='#0504aa',alpha=0.7)
+    # plt.xlim(min(bin_edges), max(bin_edges))
+    # plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('Value',fontsize=15)
+    plt.ylabel('Frequency',fontsize=15)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.ylabel('Frequency',fontsize=15)
+    plt.title('Normal Distribution Histogram',fontsize=15)
+    
+    if filepath:
+        plt.savefig(filepath)
+    else:
+        plt.show()
+        
+# print("Generating random planar traingulation - no randomization")
+# start = time.time()
+# plg = PlanarTriangulation.random_planar_triangulation(100)
+# end = time.time()
+# print(end - start)
+# print()
+
+# print("Plotting triangulation")
+# start = time.time()
+# plot_graph(plg, 'planar')
+# end = time.time()
+# print(end - start)
+# print()
+
+# # print("Sweep triangulation")
+# start = time.time()
+# plg = plg.sweep()
+# end = time.time()
+# print(end - start)
+# print()
+
+# hist = plg.number_of_neighbours()
+# print("Sweeping ", 1)
+# plg.sweep()
+# for i in range(99):
+#     hist = np.append(hist, plg.number_of_neighbours())
+#     print("Sweeping ", i+1)
+#     plg.sweep()
+
+# with open('data/histogram100x100.npy', 'wb') as f:
+#     np.save(f, hist)
+
+with open('data/histogram100.npy', 'rb') as f:
+    a = np.load(f)
+#print(a)
+print(a.shape[0])
+plot_histogram(a, "histograms/100x10000")
 
 
-plg = PlanarTriangulation.random_planar_triangulation()
-plot_graph(plg, '.')
-plg1 = PlanarTriangulation.build_triangulation(plg)
-print(plg1._area_matrix)
-plot_graph(plg1, 'plg1')
+# print("Print swept triangulation")
+# start = time.time()
+# plot_graph(plg, 'planar_swept')
+# end = time.time()
+# print(end - start)
+# print()
+
+# print("Node neighbours")
+# start = time.time()
+# hist = plg.number_of_neighbours()
+# plot_histogram(hist, "histogram")
+# end = time.time()
+# print(end - start)
+# print()
+
+# f = open("myfile.txt", "w")
+# f.write(str(hist))
+# f.close()
+# print("Finding cubic dual to triangulation")
+# start = time.time()
+# cubic, labels = plg.find_cubic_dual_to_traingulation()
+# end = time.time()
+# print(end - start)
+# print()
+
+# print("Plotting cubic")
+# start = time.time()
+# plot_graph(cubic, 'cubic', labels)
+# end = time.time()
+# print(end - start)
+# print()
+
+
+
+# area = PlanarTriangulation.randomize_edges(plg._area_matrix)
+# tg = PlanarTriangulation(PlanarTriangulation.area_matrix_to_adjacency_matrix(area),area)
+# # plg1 = PlanarTriangulation.build_triangulation(plg)
+# # print(plg1._area_matrix)
+# plot_graph(tg, 'plg1')
 
 
--- src/planar_graph.py
+++ src/planar_graph.py
@@ -5,35 +5,139 @@ import random
 import networkx as nx
 
 
-class PlanarTriangulation(Graph):
+class PlanarTriangulation():
     def __init__(self, adjacency_matrix: np.ndarray, area_matrix: np.ndarray):
         self._adjacency_matrix = adjacency_matrix.copy()
         self._area_matrix = area_matrix.copy()
 
+    def to_adjacency_matrix(self) -> np.ndarray:
+        return self._adjacency_matrix.copy()
+
+    def add_node_to_cubic(self, vertex1, vertex2, cubic: np.ndarray) -> tuple(np.ndarray, np.ndarray):
+        area_matrix = self._area_matrix
+        i1 = np.where(np.any(area_matrix == vertex1, axis=1))
+        i2 = np.where(np.any(area_matrix == vertex2, axis=1))
+        # common edge is the indexes of the rows in area_matrix that have a common edge
+        common_edge = set(i1[0]).intersection(i2[0])
+        if len(common_edge) == 1:
+            return cubic
+        node1, node2 = common_edge.pop(), common_edge.pop()
+        cubic[node1][node2] = cubic[node2][node1] = 1
+        return cubic
+
+    def find_cubic_dual_to_traingulation(self) -> tuple(Graph, dict):
+        area_matrix = self._area_matrix
+        c_nodes = area_matrix.shape[0] + 1
+        cubic_adj_m = np.zeros((c_nodes, c_nodes), dtype=np.int64)
+        mapped_areas = {tuple(row): False for row in area_matrix}
+        for key, item in mapped_areas.items():
+            cubic_adj_m = self.add_node_to_cubic(key[0], key[1], cubic_adj_m)
+            cubic_adj_m = self.add_node_to_cubic(key[1], key[2], cubic_adj_m)
+            cubic_adj_m = self.add_node_to_cubic(key[2], key[0], cubic_adj_m)
+            item = True
+
+        conn = 0
+        for i in range(cubic_adj_m.shape[0]):
+            for j in range(cubic_adj_m.shape[1]):
+                if cubic_adj_m[i][j] == 1:
+                    conn += 1
+            if conn < 3:
+                cubic_adj_m[i][-1] = cubic_adj_m[-1][i] = 1
+            conn = 0
+        labels = {}
+        for i in range(area_matrix.shape[0]):
+            lb = "[ "
+            for j in range(area_matrix.shape[1]):
+                lb += str(area_matrix[i][j]) + " "
+            lb += "]"
+            #print(lb)
+            labels[i] = lb
+            #print(f'area_matrix[{i}]={area_matrix[i]}')
+        labels[area_matrix.shape[0]] = " [ out ] "
+        #print("labels", labels)
+        return Graph(cubic_adj_m), labels
+
     def insert_vertex(area_matrix: np.ndarray) -> np.ndarray:
         areas = area_matrix.shape[0]
-        vertices =  int((areas + 5)/2)
+        vertices = int((areas + 5)/2)
         insertion = random.randint(0, areas-1)
-        print("Inserting: ", insertion)
+        #print("Inserting: ", insertion)
         vertex1, vertex2, vertex3 = area_matrix[insertion][0], area_matrix[insertion][1], area_matrix[insertion][2]
-        
+
         area_matrix[insertion] = [vertex1, vertex2, vertices]
         area_matrix = np.append(
             area_matrix, [[vertex1, vertex3, vertices]], axis=0)
         area_matrix = np.append(
             area_matrix, [[vertex2, vertex3, vertices]], axis=0)
-        
+
         return area_matrix
 
     @staticmethod
-    def build_triangulation(trian: PlanarTriangulation) -> PlanarTriangulation:
-        area_matrix = trian._area_matrix
-        vertices_to_add = 100
-        for i in range(vertices_to_add):
+    def grow_triangulation(area_matrix: np.ndarray, vertices = None) -> np.ndarray:
+        vertices -= 4
+        if vertices is None:
+            vertices = random.randint(20, 100) - 4
+        for i in range(vertices):
             area_matrix = PlanarTriangulation.insert_vertex(area_matrix)
-        
 
-        return PlanarTriangulation(PlanarTriangulation.area_matrix_to_adjacency_matrix(area_matrix), area_matrix)
+        return area_matrix
+
+    @staticmethod
+    def randomize_edge(area_matrix: np.ndarray, tries) -> np.ndarray:
+        adj_matrix = PlanarTriangulation.area_matrix_to_adjacency_matrix(
+            area_matrix)
+        if adj_matrix.shape[0] < 5:
+            print("Cannnot randomise a graph with less than 5 vertices!")
+            raise ValueError()
+        num_of_areas = area_matrix.shape[0]
+        elem1 = elem2 = vertex1 = vertex2 = row2 = row1 = -1
+        
+        row1 = random.randint(0, num_of_areas-1)
+        random_area = area_matrix[row1]
+        tmp = random.randint(0, 2)
+        vertex1 = random_area[tmp]
+        if tmp == 2:
+            vertex2 = random_area[0]
+        else:
+            vertex2 = random_area[tmp+1]
+        #print(vertex1, vertex2)
+        i1 = np.where(np.any(area_matrix == vertex1, axis=1))
+        i2 = np.where(np.any(area_matrix == vertex2, axis=1))
+        common_edge = set(i1[0]).intersection(i2[0])
+        if len(common_edge) == 0:
+            return area_matrix
+        # common_edge.remove(0)
+        row2 = common_edge.pop()
+        elem1 = np.setdiff1d(area_matrix[row1], [vertex1, vertex2])[0]
+        elem2 = np.setdiff1d(area_matrix[row2], [vertex1, vertex2])[0]
+        if not(adj_matrix[elem1][elem2] == 0 and elem1 != elem2):
+            return area_matrix
+
+        new_area1 = np.array([elem1, elem2, vertex1])
+        new_area2 = np.array([elem1, elem2, vertex2])
+        new_area1.sort()
+        new_area2.sort()
+
+        area_matrix[row1] = new_area1
+        area_matrix[row2] = new_area2
+
+        return area_matrix
+
+    def randomize_graph_edges(area_matrix: np.ndarray, num_of_rand=None) -> np.ndarray:
+        if num_of_rand is None:
+            num_of_rand = random.randint(10, 100)
+        for i in range(num_of_rand):
+            area_matrix = PlanarTriangulation.randomize_edge(area_matrix)
+        return area_matrix
+    
+    def sweep(self) -> PlanarTriangulation:
+        a_m = self._area_matrix
+        areas = a_m.shape[0]
+        edges = 3*int((areas + 5)/2) - 6
+        for i in range(edges):
+            a_m = PlanarTriangulation.randomize_edge(a_m, edges)
+        return PlanarTriangulation(PlanarTriangulation.area_matrix_to_adjacency_matrix(a_m), a_m)
+        
 
     @staticmethod
     def area_matrix_to_adjacency_matrix(area_matrix: np.ndarray) -> np.ndarray:
@@ -51,13 +155,25 @@ class PlanarTriangulation(Graph):
             adjacency_matrix[area_matrix[i][0]
                              ][prev] = adjacency_matrix[prev][area_matrix[i][0]] = 1
             prev = -1
-        print(adjacency_matrix)
-        print(area_matrix)
+        #print(adjacency_matrix)
+        #print(area_matrix)
         return adjacency_matrix
 
     @staticmethod
-    def random_planar_triangulation() -> PlanarTriangulation:
+    def random_planar_triangulation(vertices_to_add) -> PlanarTriangulation:
         area_matrix = np.array([[0, 1, 2],
                                 [0, 2, 3],
                                 [1, 2, 3],])
+        area_matrix = PlanarTriangulation.grow_triangulation(
+            area_matrix, vertices_to_add)
         return PlanarTriangulation(PlanarTriangulation.area_matrix_to_adjacency_matrix(area_matrix), area_matrix)
+
+    def number_of_neighbours(self):
+        adj_m = self.to_adjacency_matrix()
+        neigh_num = np.zeros(adj_m.shape[0], np.intc)
+        #histogram = np.zeros(adj_m.shape[0])
+        for i in range(adj_m.shape[0]):
+            for j in range(adj_m.shape[0]):
+                neigh_num[i] += adj_m[i][j]
+        #bin_edges = np.round(bin_edges,0)
+        return neigh_num
