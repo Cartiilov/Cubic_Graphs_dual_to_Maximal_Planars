5c346ae2199622b8eb63867efff70ad848beb4a1
Cartiilov <kaklimek@poczta.onet.pl>
2023-12-10 15:08:37 +0100

    Plot Gaph fix

--- src/PlotGraph.py
+++ src/PlotGraph.py
@@ -5,163 +5,234 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-
+import multiprocessing as mp
+import math
 
 
 def plot_graph(
-    graph: Graph, filepath: str | None = None, labels_dict: dict | None = None, sp: Graph | None = None
+    graph: Graph, name, labels_dict: dict | None = None, sp: Graph | None = None
 ):
-    #print(graph.to_adjacency_matrix())
-    g = nx.from_numpy_array(graph.to_adjacency_matrix())
-    if sp is not None:
-        s = nx.from_numpy_array(sp.to_adjacency_matrix())
-    #pos = nx.spring_layout(g, center=(0, 0))
 
-    plt.figure(figsize=(15, 15))
+    g = nx.from_numpy_array(graph.to_adjacency_matrix())
+    # if sp is not None:
+    #     s = nx.from_numpy_array(sp.to_adjacency_matrix())
+
+
+    plt.figure(figsize=(8, 8))
     position= nx.planar_layout(g, scale = 1, center=(0, 0))
   
     if labels_dict is not None:
-        #nx.draw_networkx_labels(labels_dict, position)
+        print(labels_dict)
         nx.draw(g, pos=position, labels = labels_dict, with_labels = True)
     else:
         nx.draw(g, pos=position)
         nx.draw_networkx_labels(g, position)
 
-    if filepath:
-        plt.savefig(filepath)
-    else:
-        plt.show()
+    filepath = 'graph_plots/trian/'
+    plt.savefig(filepath + name)
+
         
-def plot_histogram(hist: np.histogram, title: str, filepath: str | None = None):
-    h, bin_edges = np.histogram(hist)
-    plt.figure(figsize=[10,8])
-    plt.hist(hist, bins=bin_edges)
-    # plt.bar(bin_edges[:-1], hist, width = 0.5, color='#0504aa',alpha=0.7)
-    # plt.xlim(min(bin_edges), max(bin_edges))
-    # plt.grid(axis='y', alpha=0.75)
-    plt.xlabel('Value',fontsize=15)
-    plt.ylabel('Frequency',fontsize=15)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.ylabel('Frequency',fontsize=15)
-    plt.title('Normal Distribution Histogram ' + title, fontsize=15)
+def plot_distance(hist: np.histogram, title: str, filepath: str | None = None):
+
+    plt.plot(hist)
+    
+    plt.title('Histogram of number of immediate neighbours')
+    
+    plt.xlabel('Number of Occurrences', fontsize=10)
+    plt.ylabel('Number of immediate neighbours', fontsize=10)
     
     if filepath:
         plt.savefig(filepath)
     else:
         plt.show()
+    plt.clf()
+    
+def plot_distance_from_saved(name, pltname, N, col = 'blue', same_plot = False):
+    
+    with open('data/cubic/'+ name +'.npy', 'rb') as f:
+        a = np.load(f)
+    x = np.arange(0,len(a))
+    x= x.astype(float)
+
+    a = a/np.sum(a)
+    x = (x+5.5)/(pow(N,  0.25)  - 0.45)
+    plt.scatter(x, a, color=col)
+    
+    plt.xlim(0, 20)
+    plt.title('Histogram odległości')
+    
+    plt.xlabel('Odległość', fontsize=10)
+    plt.ylabel('Udział', fontsize=10)
+    plt.savefig('histograms/cubic/' + pltname)
+    
+    if not same_plot:
+        plt.clf()
+        plt.close()
         
-# print("Generating random planar traingulation - no randomization")
-# start = time.time()
-# plg = PlanarTriangulation.random_planar_triangulation(100)
-# end = time.time()
-# print(end - start)
-# print()
+# plot_distance_from_saved("next100x10000", "100 and 1000", 100, 'red', True)
+# plot_distance_from_saved("next1000x1000", "100 and 1000", 1000, 'blue', True)
+        
+def plot_neighbours_from_saved(name, pltname, log=False ,same_plot = False):
+    
+    with open('data/trian/'+ name +'.npy', 'rb') as f:
+        a = np.load(f)
+        
+    unique_values, counts = np.unique(a, return_counts=True)
+    counts = np.divide(counts, np.sum(counts))
+    
+    theory = np.ndarray( np.max(unique_values) + 1, np.float64)
+    theory[0] = None
+    theory[1] = None
+    theory[2] = None
+    
+    for k in range(3, theory.shape[0]):
+        theory[k] = 16*(pow((3.0/16), k))*(k-2)*math.factorial((2*k-2))/(math.factorial(k)*math.factorial((k-1)))
+    
 
-# print("Plotting triangulation")
-# start = time.time()
-# plot_graph(plg, 'planar')
-# end = time.time()
-# print(end - start)
-# print()
+    theory_x = np.arange(0, theory.shape[0])
 
-# # print("Sweep triangulation")
-# start = time.time()
-# plg = plg.sweep()
-# end = time.time()
-# print(end - start)
-# print()
+    plt.scatter(unique_values, counts, color='blue')
+    plt.plot(theory_x, theory, color='magenta')
+    
+    plt.title('Histogram sąsiadów')
+    
+    plt.ylabel('Udział', fontsize=10)
+    plt.xlabel('Liczba sąsiadów', fontsize=10)
+    plt.savefig('histograms/trian/' + pltname)
+    plt.clf()
+    
+    counts =-np.log10(counts)
+    theory =-np.log10(theory)
+    
+    plt.scatter(unique_values, counts, color='blue')
+    plt.plot(theory_x, theory, color='magenta')
+    
+    plt.title('Histogram sąsiadów')
+    
+    plt.ylabel('log10(Udział)', fontsize=10)
+    plt.xlabel('Liczba sąsiadów', fontsize=10)
+    plt.savefig('histograms/trian/log' + pltname)
+    
+    plt.clf()
+    plt.close()
+    
 
-# hist = plg.number_of_neighbours()
-# print("Sweeping ", 1)
-# plg.sweep()
-# for i in range(99):
-#     hist = np.append(hist, plg.number_of_neighbours())
-#     print("Sweeping ", i+1)
-#     plg.sweep()
+        
+   
+    
+def plot_multiple_neigh(name: str, *data_sets):
+    plt.clf()
+    for i in range(len(data_sets)):
+        plot_neighbours_from_saved(data_sets[i], name, True)
+    plt.clf()
+    plt.close()
+    
+def plot_multiple_dist(name: str, *data_sets):
+    plt.clf()
+    for i in range(len(data_sets)):
+        plot_distance_from_saved(data_sets[i], name, True)
+    plt.clf()
+    plt.close()
 
-# with open('data/histogram100x100.npy', 'wb') as f:
-#     np.save(f, hist)
+   
 
-with open('data/histogram100.npy', 'rb') as f:
-    a = np.load(f)
-#print(a)
-print(a.shape[0])
-plot_histogram(a, "histograms/100x10000")
-
-
-# print("Print swept triangulation")
-# start = time.time()
-# plot_graph(plg, 'planar_swept')
-# end = time.time()
-# print(end - start)
-# print()
-
-# print("Node neighbours")
-# start = time.time()
-# hist = plg.number_of_neighbours()
-# plot_histogram(hist, "histogram")
-# end = time.time()
-# print(end - start)
-# print()
-
-# f = open("myfile.txt", "w")
-# f.write(str(hist))
-# f.close()
-# print("Finding cubic dual to triangulation")
-# start = time.time()
-# cubic, labels = plg.find_cubic_dual_to_traingulation()
-# end = time.time()
-# print(end - start)
-# print()
-
-# print("Plotting cubic")
-# start = time.time()
-# plot_graph(cubic, 'cubic', labels)
-# end = time.time()
-# print(end - start)
-# print()
-
-
-
-def acquire_data(num_of_nodes, num_of_sweeps, filepath: str | None = None):
-    print("Generating random planar triangulation")
+    
+def proc_acquire(args):
+    num_of_nodes, num_of_sweeps, process_index, total_processes = args
     plg = PlanarTriangulation.random_planar_triangulation(num_of_nodes)
-    
     hist = plg.number_of_neighbours()
-    levels = plg.graph_BFS()
-    print("First sweep")
-    print("Sweeping ", 1)
+    levels = np.zeros(num_of_nodes, np.int64)
+    levels = plg.graph_BFS(levels, num_of_nodes)
+
+    start = time.time()
     plg.sweep()
-    for i in range(num_of_sweeps-1):
-        print("Get neighbours")
+    end = time.time()
+    
+    for i in range(int(num_of_sweeps/total_processes)):
+        # print("Get neighbours")
         hist = np.append(hist, plg.number_of_neighbours())
-        print("Get levels")
-        levels =  np.append(levels, plg.graph_BFS())
+        # print("Get levels")
+        levels = plg.graph_BFS(levels, num_of_nodes)
         print("Sweeping ", i+2)
         plg.sweep()
 
-    if filepath:
-        pth = filepath
-    else:
-        pth= ""
-        
-    levels = np.delete(levels, np.where(levels == 0))
-    with open('data/trian/'+ pth + str(num_of_nodes) + 'x' + str(num_of_sweeps) +'.npy', 'wb') as f:
-        np.save(f, hist)
-        
-    with open('data/cubic/'+ pth + str(num_of_nodes) + 'x' + str(num_of_sweeps) +'.npy', 'wb') as f:
-        np.save(f, hist)
+    g = nx.from_numpy_array(plg.to_adjacency_matrix())
+    if not nx.is_planar(g):
+        raise ValueError()
+    return hist, levels
+
+def acquire_data_proc(num_of_nodes, num_of_iters, num_of_processes, name: str | None = None):
+    cores_num =  mp.cpu_count()
+    if num_of_processes > cores_num:
+        msg = "Sorry, it is not optimal to run more processes than half of how many CPU cores you have! You have " + cores_num + " CPU cores"
+        raise Exception(msg)
     
+    process_args = [(num_of_nodes, num_of_iters, i, num_of_processes) for i in range(num_of_processes)]
+
+    with mp.Pool(processes=num_of_processes) as pool:
+        results = pool.map(proc_acquire, process_args)
+
+    hist = np.array([], np.int64)
+    levels = np.zeros(num_of_nodes, np.int64)
+    for i, (neigh, nlvl) in enumerate(results):
+        hist = np.append(hist, neigh)
+        levels = np.add(nlvl, levels)
+        print("lvl ", levels)
+    if not name:
+        name = ""
+    n_name = name + str(num_of_nodes) + 'x' + str(num_of_iters)
+    d_name = name + str(num_of_nodes) + 'x' + str(num_of_iters)
     
-    plot_histogram(hist, "Neighbours histogram","histograms/trian/" + pth + str(num_of_nodes) + 'x' + str(num_of_sweeps))
-    plot_histogram(levels, "Levels histogram", "histograms/cubic/" + pth + str(num_of_nodes) + 'x' + str(num_of_sweeps))
-
-acquire_data(50, 100)
-
-
-
-
-
+    with open('data/trian/'+ n_name +'.npy', 'wb') as f:
+        np.save(f, hist)
+    with open('data/cubic/'+ d_name +'.npy', 'wb') as f:
+        np.save(f, levels)
+        
+    plot_neighbours_from_saved(n_name, n_name)
+    plot_distance_from_saved(n_name, n_name, num_of_nodes)
 
 
+
+def acquire_data(num_of_nodes, num_of_iters, name) -> np.array:
+    plg = PlanarTriangulation.random_planar_triangulation(num_of_nodes)
+
+    hist = plg.number_of_neighbours()
+    levels = np.zeros(num_of_nodes, np.int64)
+    levels = plg.graph_BFS(levels, num_of_nodes)
+    print("first levels", levels)
+    print("First sweep")
+    print("Sweeping ", 1)
+    start = time.time()
+    plg.sweep()
+    end = time.time()
+    print(end - start)
+    
+    for i in range(num_of_iters - 1):
+        # print("Get neighbours")
+        hist = np.append(hist, plg.number_of_neighbours())
+        # print("Get levels")
+        levels = plg.graph_BFS(levels, num_of_nodes)
+        print("Sweeping ", i+2)
+        plg.sweep()
+
+
+    g = nx.from_numpy_array(plg.to_adjacency_matrix())
+    if not nx.is_planar(g):
+        raise ValueError()
+    
+    if not name:
+        name = ""
+        
+    n_name = name + str(num_of_nodes) + 'x' + str(num_of_iters)
+    d_name = name + str(num_of_nodes) + 'x' + str(num_of_iters)
+    
+    with open('data/trian/'+ n_name +'.npy', 'wb') as f:
+        np.save(f, hist)
+    with open('data/cubic/'+ d_name +'.npy', 'wb') as f:
+        np.save(f, levels)
+        
+    plot_neighbours_from_saved(n_name, n_name)
+    plot_distance_from_saved(n_name, n_name, num_of_nodes)
+
+
+acquire_data_proc(100, 1000, 4, 'proc')
--- src/planar_graph.py
+++ src/planar_graph.py
@@ -4,15 +4,15 @@ import numpy as np
 import random
 import networkx as nx
 import queue as q
+import time
 
 
 class PlanarTriangulation():
-    def __init__(self, adjacency_matrix: np.ndarray, area_matrix: np.ndarray):
-        self._adjacency_matrix = adjacency_matrix.copy()
+    def __init__(self, area_matrix: np.ndarray):
         self._area_matrix = area_matrix.copy()
 
     def to_adjacency_matrix(self) -> np.ndarray:
-        return self._adjacency_matrix.copy()
+        return PlanarTriangulation.area_matrix_to_adjacency_matrix(self._area_matrix)
 
     def add_node_to_cubic(self, vertex1, vertex2, cubic: np.ndarray) -> tuple(np.ndarray, np.ndarray):
         area_matrix = self._area_matrix
@@ -30,12 +30,12 @@ class PlanarTriangulation():
         area_matrix = self._area_matrix
         c_nodes = area_matrix.shape[0] + 1
         cubic_adj_m = np.zeros((c_nodes, c_nodes), dtype=np.int64)
+
         mapped_areas = {tuple(row): False for row in area_matrix}
         for key, item in mapped_areas.items():
             cubic_adj_m = self.add_node_to_cubic(key[0], key[1], cubic_adj_m)
             cubic_adj_m = self.add_node_to_cubic(key[1], key[2], cubic_adj_m)
             cubic_adj_m = self.add_node_to_cubic(key[2], key[0], cubic_adj_m)
-            item = True
 
         conn = 0
         for i in range(cubic_adj_m.shape[0]):
@@ -83,63 +83,89 @@ class PlanarTriangulation():
 
         return area_matrix
 
-    @staticmethod
-    def randomize_edge(area_matrix: np.ndarray, tries) -> np.ndarray:
-        adj_matrix = PlanarTriangulation.area_matrix_to_adjacency_matrix(
-            area_matrix)
-        if adj_matrix.shape[0] < 5:
-            print("Cannnot randomise a graph with less than 5 vertices!")
-            raise ValueError()
-        num_of_areas = area_matrix.shape[0]
-        elem1 = elem2 = vertex1 = vertex2 = row2 = row1 = -1
+    def randomize_edge(self, area_matrix: np.ndarray, area_num, adj_matrix: np.ndarray) -> tuple(np.ndarray, bool):
         
-        row1 = random.randint(0, num_of_areas-1)
+        row1 = random.randint(0, area_matrix.shape[0]-1)
         random_area = area_matrix[row1]
         tmp = random.randint(0, 2)
         vertex1 = random_area[tmp]
+        # vertex2 = np.random.choice(np.delete(random_area, np.where(random_area == vertex1)))
+        # print(random_area, vertex1, vertex2)
         if tmp == 2:
             vertex2 = random_area[0]
         else:
             vertex2 = random_area[tmp+1]
-        #print(vertex1, vertex2)
-        i1 = np.where(np.any(area_matrix == vertex1, axis=1))
+        # #print(vertex1, vertex2)
+        i1 = np.where(np.any(area_matrix == vertex1, axis=1)) 
         i2 = np.where(np.any(area_matrix == vertex2, axis=1))
         common_edge = set(i1[0]).intersection(i2[0])
         if len(common_edge) == 0:
-            return area_matrix
-        # common_edge.remove(0)
+            print("fail")
+            return area_matrix, False
+
         row2 = common_edge.pop()
-        elem1 = np.setdiff1d(area_matrix[row1], [vertex1, vertex2])[0]
+        elem1 = np.setdiff1d(random_area, [vertex1, vertex2])[0]
         elem2 = np.setdiff1d(area_matrix[row2], [vertex1, vertex2])[0]
         if not(adj_matrix[elem1][elem2] == 0 and elem1 != elem2):
-            return area_matrix
-
-        new_area1 = np.array([elem1, elem2, vertex1])
-        new_area2 = np.array([elem1, elem2, vertex2])
-        new_area1.sort()
-        new_area2.sort()
-
-        area_matrix[row1] = new_area1
-        area_matrix[row2] = new_area2
-
-        return area_matrix
-
-    def randomize_graph_edges(area_matrix: np.ndarray, num_of_rand=None) -> np.ndarray:
-        if num_of_rand is None:
-            num_of_rand = random.randint(10, 100)
-        for i in range(num_of_rand):
-            area_matrix = PlanarTriangulation.randomize_edge(area_matrix)
-        return area_matrix
+            return area_matrix, False
     
-    def sweep(self) -> PlanarTriangulation:
-        a_m = self._area_matrix
-        areas = a_m.shape[0]
-        edges = 3*int((areas + 5)/2) - 6
-        for i in range(edges):
-            a_m = PlanarTriangulation.randomize_edge(a_m, edges)
-        return PlanarTriangulation(PlanarTriangulation.area_matrix_to_adjacency_matrix(a_m), a_m)
-        
+        area1 = np.array([elem1, elem2, vertex1])
+        area2 = np.array([elem1, elem2, vertex2])
+        area1.sort()
+        area2.sort()
+        area_matrix[row1] = area1
+        area_matrix[row2] = area2
 
+        return area_matrix, True
+
+    def randomize_edge2(self, area_matrix: np.ndarray, num_of_nodes, adj_matrix: np.ndarray) -> tuple(np.ndarray, bool):
+        vertex1 = np.random.randint(0, num_of_nodes)
+        neighbours = np.where(adj_matrix[vertex1] == 1)[0]
+        vertex2 = np.random.choice(neighbours)
+
+        i1 = np.where(np.any(area_matrix == vertex1, axis = 1))
+        i2 = np.where(np.any(area_matrix == vertex2, axis = 1))
+        common_edge = np.intersect1d(i1, i2)
+
+        if len(common_edge) < 2:
+            return area_matrix, False
+        
+        area_ids = np.random.choice(common_edge, size=2, replace=False)
+        reloc1 = np.setdiff1d(area_matrix[area_ids[0]], [vertex1, vertex2])[0]
+        reloc2 = np.setdiff1d(area_matrix[area_ids[1]], [vertex1, vertex2])[0]
+        
+        if not(adj_matrix[reloc1][reloc2] == 0 and reloc1 != reloc2):
+            return area_matrix, False
+        
+        area1 = np.array([reloc1, reloc2, vertex1])
+        area2 = np.array([reloc1, reloc2, vertex2])
+        area1.sort()
+        area2.sort()
+        area_matrix[area_ids[0]] = area1
+        area_matrix[area_ids[1]] = area2
+
+        return area_matrix, True
+        
+        
+    def sweep(self) -> PlanarTriangulation:
+        area_m = self._area_matrix
+        adj_m = PlanarTriangulation.area_matrix_to_adjacency_matrix(
+            area_m)
+        nodes_num = adj_m.shape[0]
+        areas = area_m.shape[0]
+        edges = 3*int((areas + 5)/2) - 6
+        was_changed = False
+        if nodes_num < 5:
+            print("Cannnot randomise a graph with less than 5 vertices!")
+            raise ValueError()
+        for i in range(edges):
+            area_m, was_changed = self.randomize_edge(area_m, nodes_num, adj_m)
+            
+            if was_changed:
+                adj_m = PlanarTriangulation.area_matrix_to_adjacency_matrix(
+                area_m)
+        return PlanarTriangulation(area_m)
+        
     @staticmethod
     def area_matrix_to_adjacency_matrix(area_matrix: np.ndarray) -> np.ndarray:
         areas = area_matrix.shape[0]
@@ -159,13 +185,15 @@ class PlanarTriangulation():
         return adjacency_matrix
 
     @staticmethod
-    def random_planar_triangulation(vertices_to_add) -> PlanarTriangulation:
+    def random_planar_triangulation(number_f_nodes) -> PlanarTriangulation:
+        
         area_matrix = np.array([[0, 1, 2],
                                 [0, 2, 3],
-                                [1, 2, 3],])
+                                [1, 2, 3]])
+        
         area_matrix = PlanarTriangulation.grow_triangulation(
-            area_matrix, vertices_to_add)
-        return PlanarTriangulation(PlanarTriangulation.area_matrix_to_adjacency_matrix(area_matrix), area_matrix)
+            area_matrix, number_f_nodes)
+        return PlanarTriangulation(area_matrix)
 
     def number_of_neighbours(self):
         adj_m = self.to_adjacency_matrix()
@@ -178,20 +206,27 @@ class PlanarTriangulation():
         return neigh_num
     
         
-    def graph_BFS(self) -> np.ndarray:
-        g, d = self.find_cubic_dual_to_triangulation()
-        num_of_nodes = g.to_adjacency_matrix().shape[0]
-        node_neigh = np.zeros(num_of_nodes, dtype=np.int64)
-        for i in range(num_of_nodes):
-            node_neigh = np.append(node_neigh, PlanarTriangulation.BFS(g, i, num_of_nodes))
-        return node_neigh
-  
-    @staticmethod
-    def BFS(g: Graph, node, num_of_nodes) -> np.ndarray:
-        level = np.empty(num_of_nodes)
-        level[:] = np.NaN
-        marked = np.full(num_of_nodes, False)
+    def graph_BFS(self, histogram: np.ndarray, num_of_nodes) -> np.ndarray:
+        # randomlist = random.sample(range(0, num_of_nodes-1), int(0.05*num_of_nodes))
+        adj_matrix = self.to_adjacency_matrix()
+        num_of_nodes = adj_matrix.shape[0]
+         #find dual graph
+        g = Graph(adj_matrix)
 
+        adj_list = g.to_adjacency_list() # find adjacency list for graph
+        for i in range(num_of_nodes-1): # call BFS on every node
+            # histogram ->BFS works on one histogram for all iterations
+            histogram = PlanarTriangulation.BFS(i, num_of_nodes, adj_list, histogram) 
+
+        return histogram
+  
+    #BFS accepts graph, number of node on which it wil start, adjacency list and histogram
+    #histogram stores the data of how many nodes are on each level
+    @staticmethod
+    def BFS(node, num_of_nodes, adj_list: np.ndarray, histogram: np.ndarray) -> np.ndarray:
+        level = np.zeros(num_of_nodes, np.int32)
+        marked = np.full(num_of_nodes, False)
+        
         que = q.Queue()
         que.put(node)
 
@@ -199,17 +234,22 @@ class PlanarTriangulation():
         marked[node] = True
         
         while (not que.empty()):
+            
             node = que.get()
             
-            adj_l = g.to_adjacency_list()[node]
+            adj_l = adj_list[node]
             for i in range(len(adj_l)):
 
                 b = adj_l[i]
 
                 if (not marked[b]):
-
+                    
                     que.put(b)
+                    #establish on which level the node is
                     level[b] = level[node] + 1
+                    
+                    #add one to the n8umber of occurences for a ceartain distance
+                    histogram[level[b]] += 1
+                    
                     marked[b] = True
-        
-        return level
+        return histogram
